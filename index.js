839ccc76dc0f8232393100dd917f8c44f862b09f
angeldev07 <angelgaran20@gmail.com>
2023-03-02 11:09:42 -0500

    Establecida la conexion a la base de datos

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const { dbConnection } = require('./DB/config');
+
 // leemos el .env
 require('dotenv').config()
 // Mi servidor
 const app = express();
+
+//DB connection 
+dbConnection();
 
 // Directorio publico
 app.use( express.static('public'))
