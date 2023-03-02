08e7322181e8e610d2759656112a74338fbe1f4e
angeldev07 <angelgaran20@gmail.com>
2023-03-02 11:10:16 -0500

    Creado archivo de configuracion para la conexion a la bd

---
+++ DB/config.js
@@ -0,0 +1,17 @@
+
+const mongoose = require('mongoose')
+
+const dbConnection  = async () => {
+    try {
+
+        await mongoose.connect(process.env.BD_CNN);
+        console.log('Base de datos montada');
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error a la hora de inicializar la base de datos');
+    }
+}
+
+module.exports = {
+    dbConnection
+}
